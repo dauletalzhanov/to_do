b08ddd0177f3fb649ba2b9ee3f4ecbb8a62d6850
dauletalzhanov <35165187+dauletalzhanov@users.noreply.github.com>
2024-05-15 15:05:03 +0100

    adding assets

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,26 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin")
 const path = require("path")
+
+const toml = require("toml")
+const yaml = require("yamljs")
+const json5 = require("json5")
 
 module.exports = {
 	mode: "development",
+	
 	entry: "./src/index.js",
 	output: {
+		filename: '[name].bundle.js',
 		path: path.resolve(__dirname, "dist"),
+		clean: true,
 	},
+
+	optimization: {
+		runtimeChunk: 'single',
+	},
+	plugins: [new HtmlWebpackPlugin({
+		title: "To-Do List!"
+	})],
 
 	devServer: {
 		static: {
@@ -17,13 +32,43 @@ module.exports = {
 		compress: true,
 		historyApiFallback: true,
 	},
+	
 	module: {
 		rules: [
 			{
 				test: /\.css$/i,
 				include: path.resolve(__dirname, "src"),
 				use:["style-loader", "css-loader", "postcss-loader"]
-			}
+			},
+			{
+				test: /\.(png|svg|jpg|jpeg|gif)$/i,
+				type: "asset/resource"
+			},
+			{
+				test: /\.(woff|woff2|eot|ttf|otf)$/i,
+				type: 'asset/resource',
+			},
+			{
+				test: /\.toml$/i,
+				type: 'json',
+				parser: {
+				  parse: toml.parse,
+				},
+			},
+			{
+				test: /\.yaml$/i,
+				type: 'json',
+				parser: {
+				  parse: yaml.parse,
+				},
+			},
+			{
+				test: /\.json5$/i,
+				type: 'json',
+				parser: {
+				  parse: json5.parse,
+				},
+			},
 		],
 	}
 }
